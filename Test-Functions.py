aecd0b8d2fb52d83a38804c6addf4fd5b581b193
Fusilladin <91526142+Fusilladin@users.noreply.github.com>
2021-09-29 18:36:26 -0500

    Add files via upload

---
+++ Test-Functions.py
@@ -0,0 +1,13 @@
+def two_plus_two():
+    val = 2 + 2
+    return val
+
+sum = (two_plus_two() * 2)
+print(sum)
+
+# print multiple numbers in functions
+def multiply_two_nums(num1, num2):
+    val = num1 * num2
+    return val
+
+print(multiply_two_nums(5, 10))
